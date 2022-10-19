e5865702ceb471c1c5fe54e0fe8a0367d811a667
xprezzo-marco <102872031+xprezzo-marco@users.noreply.github.com>
2022-10-19 13:28:17 +0200

    correct

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,25 +8,19 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace XPComboTest.Properties
-{
-
-
+namespace XPComboTest.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
             }
         }
-
-        public object ConfigDir { get; internal set; }
     }
 }
